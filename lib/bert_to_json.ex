33d8655949ca6640115b1bb74184975a6a015922
EmilienMottet <emilien.mottet@grenoble-inp.org>
2022-11-07 17:53:34 +0100

    init

---
+++ lib/bert_to_json.ex
@@ -0,0 +1,18 @@
+defmodule BertToJson do
+  @moduledoc """
+  Documentation for `BertToJson`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BertToJson.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bert_to_json_test.exs
@@ -0,0 +1,8 @@
+defmodule BertToJsonTest do
+  use ExUnit.Case
+  doctest BertToJson
+
+  test "greets the world" do
+    assert BertToJson.hello() == :world
+  end
+end
